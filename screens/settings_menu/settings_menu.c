d24c908602574e1ec31cf5739814b014fc853a46
SimonRmblt <simon.riembault@epitech.eu>
2022-05-02 19:47:37 +0200

    [~] Removing test tiles

--- screens/settings_menu/settings_menu.c
+++ screens/settings_menu/settings_menu.c
@@ -48,21 +48,6 @@ void create_sprite_settings(scene *d)
     create_sprite_sb(d, "white_sb",
      create_btn_param(create_vector2f(600, 80), 2, 4, HOME_BTN),
         my_int_to_str(d->hub->s->volume));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 350), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 450), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 550), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 650), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 750), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 850), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 950), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 1050), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 1150), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 1250), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 1350), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 1450), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 1550), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 1650), 1, 4, SCROLL_BTN));
-    create_sprite_ltile(d, "white_lt", create_btn_param(create_vector2f(1100, 1750), 1, 4, SCROLL_BTN));
 }
 
 void data_settings(screen *screen1)
