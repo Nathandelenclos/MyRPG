d2af76f3f499b89eb47c96f06357fd3a3ed594bd
David PLOUVIER <david.plouvier@epitech.eu>
2022-05-06 02:39:29 +0200

    [+] save backup branch

--- sprites/player/player.c
+++ sprites/player/player.c
@@ -7,6 +7,46 @@
 
 #include "../../include/rpg.h"
 
+void regeneration_player(game_obj *g, scene *d)
+{
+    // game_obj *map = get_object(d, "maps");
+    // slime *s = (slime *) g->data;
+    game_obj *p = get_object(d, "player");
+    sfVector2f pos_map = sfSprite_getPosition(map->sprite);
+    pos_map.x += g->vector.x * 9.0;
+    pos_map.y += g->vector.y * 9.0;
+    sfSprite_setPosition(g->sprite, pos_map);
+    pos_map.y -= 10;
+    pos_map.x += 30;
+    float distance = get_distance(g, p);
+    s->time = sfClock_getElapsedTime(g->clock);
+    float seconds = sfTime_asSeconds(s->time);
+    float old_seconds = sfTime_asSeconds(s->old_time_hit);
+    if (distance <= 150.0)
+        s->state = HIT;
+    else
+        s->state = IDLE;
+    if (g->display)
+        print_life_bar(d, s->lb, s->hp * 10, pos_map);
+    if (g->display && distance <= 150.0 && seconds - old_seconds >= 0.5) {
+        ((player *) p->data)->hp -= 2;
+        s->old_time_hit = sfClock_getElapsedTime(g->clock);
+        if (((player *) p->data)->hp <= 0) {
+            ((player *) p->data)->hp = 100;
+            switch_scene(d, START);
+        }
+    }
+    if (s->hp <= 0)
+        s->state = DESTROY;
+    
+
+    game_obj *map = get_object(d, "maps");
+    player *s = (player *) g->data;
+    sfVector2f pos_map = sfSprite_getPosition(map->sprite);
+    pos_map.x += g->vector.x * 9.0;
+    pos_map.y += g->vector.y * 9.0;
+}
+
 void print_life_bar_player(scene *d, player *p)
 {
     life_percent(p->lb, p->hp);
